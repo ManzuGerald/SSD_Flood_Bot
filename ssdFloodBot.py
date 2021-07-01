a0c96cdf58b277362f1fa6186ff33d12aac92827
Manzu Gerald <manzugerald@gmail.com>
2021-07-01 10:43:01 +0900

    Added comments to code

--- ssdFloodBot.py
+++ ssdFloodBot.py
@@ -14,39 +14,57 @@ CONSUMER_SECRET = environ['CONSUMER_SECRET']
 ACCESS_KEY = environ['ACCESS_KEY']
 ACCESS_SECRET = environ['ACCESS_SECRET']
 
+###********** Authentication **********
 auth = tweepy.OAuthHandler(CONSUMER_KEY,CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY,ACCESS_SECRET)
 api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
-#wait_on_rate_limit monitors and lets the API wait when we hit some limits.
 
+###********** Build a streamListener class that will listen to tweets based on a track list **********
 class StreamListener(tweepy.StreamListener):
+    #The first method is the constructor, which takes the API for Authentication
     def __init__(self, api):
         self.api = api
         self.me = api.me()
+
+    #There are mainly 2 methods
+    #The on_data methods is good for returning or updating a data file. on_status does the opposite
     def on_status(self, status):
         #Ignores the tweet so long as I am the Author, or it's a reply to a tweet
         if status.in_reply_to_status_id is not None or \
             status.user.id == self.me.id:
             return
+
+        #Otherwise, if a tweet that meets the criteria isn't liked yet, then it should be liked *favourite()*
+
+        #if not status.favorited:
+            #try:
+                #status.favorite()
+
+            #except Exception as e:
+                #print("Error from liking")
+                #print("Error on_data %s" % str(e))
+                #return True
+        #If the tweet is not retweeted, retweet() method is called.
         if not status.retweeted:
             try:
                 status.retweet()
-            except:
-                print("Retweet Error")
+            except Exception as e:
+                print("Error on_data %s" % str(e))
+                print("Error from retweeting")
                 return True
-        print(status.text)
+        print(status.text) #Prints the text of the tweet. This can be commented out after deploying
         #print(dir(status))
-
-        def on_error(self, status_code):
-            if status_code == 420:
-                print("Error on_data %s" % str(e))
-                print("Error from limits")
-                return True
-            if status_code == 429:
-                print("Error on_data %s" % str(e))
-                print("LIMIT EXCEEDED")
-                return True
-
+    #status code 420 prevents our code from reaching Twitter vv limits and warnings.....
+    def on_error(self, status_code):
+        if status_code == 420:
+            print("Error on_data %s" % str(e))
+            print("Error from limits")
+            return True
+        if status_code == 429:
+            print("Error on_data %s" % str(e))
+            print("LIMIT EXCEEDED")
+            return True
+            
 stream_listener = StreamListener(api)
 stream = tweepy.Stream(auth=api.auth, listener=stream_listener)
 ssd_list = ["@ssdFloodBot","flooding south sudan","south sudan flooding", "south sudan floods", "floods south sudan", "floods in south sudan","south sudan in floods", "flooding in south sudan", "flooding bor", "south sudan flooding", "bor flooding", "flooding akobo", "AKOBO FLOODING"]
