e1d29f25719f39518affba109487cf080c099c0a
Manzu Gerald <manzugerald@gmail.com>
2021-07-01 11:01:09 +0900

    Added more readability

--- ssdFloodBot.py
+++ ssdFloodBot.py
@@ -65,6 +65,6 @@ class StreamListener(tweepy.StreamListener):
 
 stream_listener = StreamListener(api)
 stream = tweepy.Stream(auth=api.auth, listener=stream_listener)
-ssd_list = ["flooding south sudan","south sudan flooding", "south sudan floods", "floods south sudan", "floods in south sudan","south sudan in floods", "flooding in south sudan", "flooding bor", "south sudan flooding", "bor flooding", "flooding akobo", "AKOBO FLOODING", "@ssdFloodBot"]
+ssd_list = ["flooding south sudan","south sudan flooding", "south sudan floods", "floods south sudan", "floods in south sudan","south sudan in floods", "flooding in south sudan", "flooding bor", "south sudan flooding", "@ssdFloodBot"]
 
 stream.filter(track= ssd_list)
